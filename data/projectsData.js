d23b8d9910701d7f50420ec6492cebf97e13245b
Cengizhan <97846543+UchihaCengiz34@users.noreply.github.com>
2022-01-19 22:09:13 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
-    title: 'Naruto Animesinin Konusu',
-    description: `Uzumaki Naruto isimli bir cocugun Uchiha Sasukeyi geri getirmeye calismasiyla basliyor hikayemiz gerisini izleseniz daha iyi:)`,
+    title: 'NarutoAnimesininKonusu',
+    description: `Uzumaki naruto isimli bir cocugun Uchiha Sasukeyi geri getirmeye calismasiyla basliyor hikayemiz gerisini izleseniz daha iyi:)`,
     imgSrc: ''
   },
 ]
