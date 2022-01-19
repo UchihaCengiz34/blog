51af2431b24a3f2bc9c658cb8375613b924d1304
Cengizhan <97846543+UchihaCengiz34@users.noreply.github.com>
2022-01-19 21:54:28 +0300

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Anime Hakkinda Yanlis Bilinenler' },
   { href: '/tags', title: 'Karakter biyografileri' },
   { href: '/projects', title: 'Neden Naruto?' },
-  { href: '/about', title: 'Hakkımda' },
+  { href: '/about', title: 'Hakkimda' },
 ]
 
 export default headerNavLinks
